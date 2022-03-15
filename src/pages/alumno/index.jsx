623ecbde6d511ef1e4911f133da64b53d16e2b68
Luis Garmendia <lf_fernandezg@unicah.edu>
2022-03-14 18:27:30 -0600

    Update index.jsx

--- src/pages/alumno/index.jsx
+++ src/pages/alumno/index.jsx
@@ -42,6 +42,7 @@ const validar = (values) => {
 
 const onSubmit = (values, { setSubmitting }) => {
   alert(JSON.stringify(values, null, 2));
+  console.log('values', values)
   setTimeout(() => {
     setSubmitting(false);
   }, 400);
@@ -57,7 +58,7 @@ export const Students = () => {
             validate={validar}
             onSubmit={onSubmit}
           >
-            {({ errors, isSubmitting }) => (
+            {({ errors, isSubmitting,isValid,resetForm }) => (
               <Form>
                 <h1>Alumno</h1>
                 <div className="form-group">
@@ -94,7 +95,9 @@ export const Students = () => {
                     name="identity"
                     placeholder="Identidad"
                     className="form-control input-sm "
+                    
                   />
+                  
                   <ErrorMessage
                     name="identity"
                     component={() => <ErrorInput error={errors.identity} />}
@@ -170,9 +173,17 @@ export const Students = () => {
                 <button
                   type="submit"
                   className="btn btn-primary"
-                  disabled={isSubmitting}
+                  disabled={isSubmitting || !isValid}
                 >
                   Submit
+                </button>
+
+                <button
+                  type="reset"
+                  className="btn btn-warning"
+                  onClick={resetForm}
+                >
+                  Limpiar
                 </button>
               </Form>
             )}
@@ -217,97 +228,6 @@ export const Students = () => {
           </table>
         </div>
       </div>
-
-      {/* <form action="#" className="p-3 form">
-        <div className="container">
-          <div className="row justify-content-center">
-            <div className="col-12 col-sm-6 col-md-6 col-xl-6">
-                <div className="form-group">
-                  <label htmlFor="inputsm">Nombre Completo:</label>
-                  <input type="text"  placeholder="Nombre Completo" className="form-control input-sm" id="inputsm"/>
-                </div>
-                
-                <div className="form-group">
-                  <label htmlFor="inputdefault">Identidad:</label>
-                  <input type="text" placeholder="Identidad" className="form-control" id="inputdefault"/>
-                </div>
-                
-                <div className="form-group">
-                  <label htmlFor="inputlg">Corrreo</label>
-                  <input type="text" placeholder="Correo" className="form-control input-lg" id="inputlg"/>
-                </div>
-
-                <div className="form-group">
-                  <label htmlFor="inputlg">Corrreo</label>
-                  <input type="password" placeholder="Correo" className="form-control input-lg" id="inputlg"/>
-                </div>
-
-                <div className="form-group">
-                  <label htmlFor="inputlg">Confirmar contraseña</label>
-                  <input type="password" placeholder="Correo" className="form-control input-lg" id="inputlg"/>
-                </div>
-
-
-                <div className="form-group">
-                    <label htmlFor="inputlg">Seleccione la Carrera:</label>
-                    <div>
-                    <select name="cars" id="cars">
-                        <optgroup label="Ingenierias">
-                            <option value="Computacion">Ciencias de la Computacion</option>
-                            <option value="Administracion">Adminitración y Gestion Estrategica</option>
-                            <option value="Civil">Civil</option>                          
-                        </optgroup>
-
-                        <optgroup label="Licenciaturas">
-                            <option value="Derecho">Derecho</option>
-                            <option value="Phicologia">Phicologia</option>
-                            <option value="Mercadotecnia">Mercadotecnia</option>
-                            <option value="CirujuaDental">Cirugía Dental</option>
-                        </optgroup>
-                        
-                        <optgroup label="Doctorado">
-                            <option value="Medicina">Medicina</option>
-                        </optgroup>
-                    </select>
-                    </div>
-                </div>
-              </div>
-              <div className="col-12 col-sm-6 col-md-6 col-xl-6">
-              <h1>Alumnos</h1>
-              <table id="alumno" className="table is-striped" style={{width:'100%'}}>
-                  <thead>
-                      <tr>
-                          <th>Name</th>
-                          <th>Position</th>
-                          <th>Office</th>
-                          <th>Age</th>
-                          <th>Start date</th>
-                          <th>Salary</th>
-                      </tr>
-                  </thead>
-                  <tbody>
-                      <tr>
-                          <td>Tiger Nixon</td>
-                          <td>System Architect</td>
-                          <td>Edinburgh</td>
-                          <td>61</td>
-                          <td>2011/04/25</td>
-                          <td>$320,800</td>
-                      </tr>
-                      <tr>
-                          <td>Garrett Winters</td>
-                          <td>Accountant</td>
-                          <td>Tokyo</td>
-                          <td>63</td>
-                          <td>2011/07/25</td>
-                          <td>$170,750</td>
-                      </tr>
-                    </tbody>
-                </table>
-              </div>
-          </div>
-        </div>
-      </form> */}
     </Split>
   );
 };
